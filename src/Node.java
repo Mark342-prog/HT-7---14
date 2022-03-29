87b857a151466b38f3432f0f5b18a6420e0c9658
CindyGualim <gua21226@uvg.edu.gt>
2022-03-29 10:54:07 -0600

    Asociación de nodos

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+/***
+ *
+ * Referencia para su Implementación
+ * https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+ *
+ */
+class Node {
+    Association data;
+    Node left, right;
+
+    Node(Association data) {
+        this.data = data;
+        left = right = null;
+    }
+}
